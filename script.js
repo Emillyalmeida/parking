8d6dedcfe2bbde8e46698fbb10175b6081270e54
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-14 13:22:50 -0300

    faet: capture data form ok

--- script.js
+++ script.js
@@ -2,4 +2,22 @@
 const form = document.querySelector("form");
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const dataCadastro = {
+        name: "",
+        placa: "",
+        type: "",
+        entrace: new Date(),
+    };
+    const inputs = event.target;
+    for (let i = 0; i < (inputs === null || inputs === void 0 ? void 0 : inputs.length); i++) {
+        const { name, value } = inputs[i];
+        if (name) {
+            name === "name"
+                ? (dataCadastro.name = value)
+                : name === "type"
+                    ? (dataCadastro.type = value)
+                    : (dataCadastro.placa = value);
+        }
+    }
+    console.log(dataCadastro);
 });
--- script.ts
+++ script.ts
@@ -1,5 +1,37 @@
 const form: HTMLFormElement | null = document.querySelector("form");
 
+interface vehicle {
+  name: string;
+  placa: string;
+  type: string;
+  entrace: Date;
+}
+
 form?.addEventListener("submit", (event) => {
   event.preventDefault();
+  const dataCadastro: vehicle = {
+    name: "",
+    placa: "",
+    type: "",
+    entrace: new Date(),
+  };
+  const inputs = event.target as HTMLFormElement;
+
+  for (let i = 0; i < inputs?.length; i++) {
+    const { name, value } = inputs[i] as HTMLInputElement;
+    if (name) {
+      name === "name"
+        ? (dataCadastro.name = value)
+        : name === "type"
+        ? (dataCadastro.type = value)
+        : (dataCadastro.placa = value);
+    }
+  }
+  console.log(dataCadastro);
 });
+
+class Actions {
+  static AddVehicle() {}
+  static Render() {}
+  static Delete() {}
+}
