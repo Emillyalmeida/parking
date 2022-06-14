21e596e8ab026703fcf29120d99ae5ad023aa27a
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-14 14:06:47 -0300

    feat: init methods Render, getVehicle and AddVehivle

--- script.ts
+++ script.ts
@@ -27,11 +27,39 @@ form?.addEventListener("submit", (event) => {
         : (dataCadastro.placa = value);
     }
   }
-  console.log(dataCadastro);
+  Actions.AddVehicle(dataCadastro);
 });
 
 class Actions {
-  static AddVehicle() {}
-  static Render() {}
+  static AddVehicle(vehicle: vehicle) {
+    const listVehicles = this.getVehicle();
+    const update = [...listVehicles, vehicle];
+    localStorage.setItem("parking", JSON.stringify(update));
+
+    this.Render(update);
+  }
+  static getVehicle() {
+    return localStorage.parking ? JSON.parse(localStorage.parking) : [];
+  }
+  static Render(vehicles: vehicle[]) {
+    const tbody: HTMLElement | null = document.getElementById("vehicles");
+    tbody!.innerHTML = "";
+
+    vehicles.map((vehicle) => {
+      const { name, placa, entrace, type } = vehicle;
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+       <td>${name}</td>
+       <td>${placa}</td>
+       <td>${type}</td>
+       <td>${entrace}</td>
+       <td>
+          <button class='delete' id=${placa}>X</button>
+       </td>
+    `;
+      tbody?.appendChild(row);
+    });
+  }
   static Delete() {}
 }
